0d6081b97eb5ee14007d76aa938511b7b3af16c8
Leonardo de Oliveira <leonardo.oliveira@fulltime.com.br>
2026-03-19 13:44:41 -0300

    feat: Adição de index na tabela e parametro onupdate para update_at

--- domain/entities/models/tarefa.py
+++ domain/entities/models/tarefa.py
@@ -1,10 +1,16 @@
 from datetime import datetime
-from sqlalchemy import String, DateTime, func
+from sqlalchemy import Index, String, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 from infrastructure.database_mysql.mysql_connection import Base
 
 class TarefaModel(Base):
     __tablename__ = "tarefas"
+    __table_args__ = (
+        Index('idx_tarefas_titulo', "titulo"),
+        
+        #Indice composto
+        Index('idx_tarefas_concluida_created', "concluido", "created_at"),
+    )
     
     
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -12,7 +18,7 @@ class TarefaModel(Base):
     descricao: Mapped[str] = mapped_column(String(250), nullable=False)
     concluido: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    update_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    update_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now)
     
     def __repr__(self) -> str:
         return f"TarefaModel(id={self.id}, titulo='{self.titulo}')"
